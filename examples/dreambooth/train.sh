d45c39805a59908b23b8abe11a83c123c74f9a75
qilei123 <qileimail123@gmail.com>
2023-03-21 23:08:49 +0000

    test dreambooth

---
+++ examples/dreambooth/train.sh
@@ -0,0 +1,18 @@
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export INSTANCE_DIR="/home/qilei/DEVELOPMENT/diffusers/datasets/"
+export OUTPUT_DIR="output/DreamBoothDataset4Med/"
+
+accelerate launch  --mixed_precision="fp16" train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --dataset="DreamBoothDataset4Med" \
+  --instance_prompt="a photo of gastroscopy disease" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --sample_batch_size=1 \
+  --gradient_accumulation_steps=1 --gradient_checkpointing \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=400
